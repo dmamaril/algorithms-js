d4cf02f2dd5ccbb54490d3c3efe4b0f14ed242a7
Don Mamaril <mamarildon@gmail.com>
2016-06-22 11:01:55 -0700

    [wip] graphs: implement removeEdge

--- graph.js
+++ graph.js
@@ -2,7 +2,12 @@ var Graph = function(){
 	this._adjacencyList = {};
 };
 
-Graph.prototype.addNode = function(newNode, toNode){
+/**
+ * [addNode description]
+ * @param {[type]} newNode [description]
+ * @param {[type]} toNode  [description]
+ */
+Graph.prototype.addNode = function addNode(newNode, toNode){
 
 	if (newNode === undefined || newNode === null) {
 		return null;
@@ -20,17 +25,32 @@ Graph.prototype.addNode = function(newNode, toNode){
 	return this;
 };
 
-Graph.prototype.contains = function(node){
+/**
+ * [contains description]
+ * @param  {[type]} node [description]
+ * @return {[type]}      [description]
+ */
+Graph.prototype.contains = function contains(node){
 	return !!this._adjacencyList[node];
 };
 
-Graph.prototype.removeNode = function(node){
+/**
+ * [removeNode description]
+ * @param  {[type]} node [description]
+ * @return {[type]}      [description]
+ */
+Graph.prototype.removeNode = function removeNode(node){
 };
 
-Graph.prototype.getEdge = function(fromNode, toNode){
+Graph.prototype.getEdge = function getEdge(fromNode, toNode){
 };
 
-Graph.prototype.addEdge = function(fromNode, toNode){
+/**
+ * [addEdge description]
+ * @param {[type]} fromNode [description]
+ * @param {[type]} toNode   [description]
+ */
+Graph.prototype.addEdge = function addEdge(fromNode, toNode){
 
 	// fromNode and toNode must exist to continue;
 	if (fromNode === undefined || fromNode === null) {
@@ -47,15 +67,59 @@ Graph.prototype.addEdge = function(fromNode, toNode){
 	return this;
 };
 
-Graph.prototype.removeEdge = function(fromNode, toNode){
+/**
+ * [removeEdge description]
+ * @param  {[type]} fromNode [description]
+ * @param  {[type]} toNode   [description]
+ * @return {[type]}          [description]
+ */
+Graph.prototype.removeEdge = function removeEdge(fromNode, toNode){
+
+	if (fromNode === undefined || fromNode === null) {
+		return null;
+	}
+
+	if (toNode === undefined || toNode === null) {
+		return null;
+	}
+
+	// if either dont exist
+	if (!!this._adjacencyList[fromNode] || !!this._adjacencyList[toNode]) {
+		return null;
+	}
+
+	var fromIndex 	= this._adjacencyList[fromNode].indexOf(toNode);
+	var toIndex 	= this._adjacencyList[toNode].indexOf(fromNode);
+
+	if (fromIndex !== -1) {
+		this._adjacencyList[fromNode].splice(fromIndex, 1);
+	}
+
+	if (toIndex !== -1) {
+		this._adjacencyList[toNode].splice(toIndex, 1);
+	}
+
+	// should i be killing the nodes just because they dont have edges?
+	// leave commented in the mean time; the answer should be NO;
+	// !this._adjacencyList[fromNode].length && this.removeNode(fromNode);
+	// !this._adjacencyList[toNode].length && this.removeNode(toNode);
+
+	return this;
 };
 
-
-Graph.prototype.depthFirstTraversal = function() {
+/**
+ * [depthFirstTraversal description]
+ * @return {[type]} [description]
+ */
+Graph.prototype.depthFirstTraversal = function depthFirstTraversal() {
 
 };
 
-Graph.prototype.breadthFirstTraversal = function () {
+/**
+ * [breadthFirstTraversal description]
+ * @return {[type]} [description]
+ */
+Graph.prototype.breadthFirstTraversal = function breadthFirstTraversal () {
 
 };
 
