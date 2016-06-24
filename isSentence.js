7381dad3acdf29f6b6626f2ea65e10f07f8348e6
Don Mamaril <mamarildon@gmail.com>
2016-06-23 17:24:50 -0700

    [update] isSentence: include trie approach; analyze time complexities

--- isSentence.js
+++ isSentence.js
@@ -40,16 +40,101 @@ var dictionary = {
 	max: 1
 };
 
-isSentenceDP('iamuber');
-// isSentenceDP('ixam');
+debugger;
+isSentenceDP('isauberx');
+// isSentenceDP('imaxxuber');
 
+// trie approach
+// function isSentence(str) {
+
+// 	var memo = {};
+
+// 	// build trie;
+// 	for ( var i = 0 ; i < str.length ; i++ ) {
+
+// 		var letter = str[i];
+
+// 		if (!!dictionary[letter] && !memo[letter]) {
+// 			memoize(letter);
+// 		}
+
+// 		var word = letter;
+
+// 		for (var j = i+1 ; j < str.length ; j++) {
+
+// 			word += str[j];
+
+// 			if (!!dictionary[word]) {
+// 				memoize(letter, word);
+// 			}
+// 		}
+// 	}
+
+
+// 	var first_letter = str[0];
+// 	var first_words  = memo[first_letter];
+
+// 	// no valid words at starting letter just end;
+// 	if (!memo[first_letter]) {
+// 		return false;
+// 	}
+
+// 	// try to build out the sentence with each starting word;
+// 	for (i = 0 ; i < first_words.length ; i++) {
+// 		if (recurse(first_words[i], first_words[i].length-1)) {
+// 			return true;
+// 		}
+// 	}
+
+// 	return false;
+
+// 	*
+// 	 * [memoize description]
+// 	 * @param  {[type]} letter [description]
+// 	 * @param  {[type]} word   [description]
+// 	 * @return {[type]}        [description]
+	 
+// 	function memoize(letter, word) {
+// 		memo[letter] = memo[letter] || [];
+// 		memo[letter].push(word || letter);
+// 	}
+
+// 	/**
+// 	 * [recurse description]
+// 	 * @param  {[type]} prevWord  [description]
+// 	 * @param  {[type]} rootIndex [description]
+// 	 * @return {[type]}           [description]
+// 	 */
+// 	function recurse(prevWord, rootIndex) {
+
+// 		if (rootIndex === str.length-1) {
+// 			return true;
+// 		}
+
+// 		var rootLetter = str[rootIndex + 1];
+
+// 		if (!memo[rootLetter] || rootIndex >= str.length) {
+// 			return false;
+// 		}
+
+// 		var words = memo[rootLetter];
+
+// 		for (var i = 0 ; i < words.length ; i++) {
+// 			if(recurse(words[i], rootIndex + words[i].length)) {
+// 				return true;
+// 			}
+// 		}
+
+// 		return false;
+// 	}
+// }
 
 // initial implementation;
 // function isSentence(str) {
     
 //     function findSentences(index, word) {
     
-//         if (dictionary[word] && index === str.length-1) {
+//         if (isInDict(word) && index === str.length-1) {
 //                return true;
 //         }
         
@@ -59,7 +144,7 @@ isSentenceDP('iamuber');
         
 //         index++;
 //         var letter = str[index];
-//         return dictionary[word] ? findSentences(index, letter) : findSentences(index, word + letter);
+//         return isInDict(word) ? findSentences(index, letter) : findSentences(index, word + letter);
 //     }
 
 //     return findSentences(0, str[0]);
