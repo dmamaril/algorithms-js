f3791fac53470ae8769c17a04a8cb3dcfa2f7770
Don Mamaril <mamarildon@gmail.com>
2016-07-18 22:35:08 -0700

    [feat] numDecodings: LC # 91 M O(n)

--- 091. Decode Ways.js
+++ 091. Decode Ways.js
@@ -12,6 +12,7 @@
  * Given an encoded message containing digits, determine the total number of ways to decode it.
  * 
  * For example,
+ * 
  * Given encoded message "12", it could be decoded as "AB" (1 2) or "L" (12).
  * 
  * The number of ways decoding "12" is 2.
